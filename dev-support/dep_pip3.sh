991ec7638f854c9b84c7a79124f2462a4f5837d1
bdmd <422924256@qq.com>
2025-03-11 11:02:16 +0800

    Update dep_pip3.sh

--- dev-support/dep_pip3.sh
+++ dev-support/dep_pip3.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 set -e
+echo "exec env:"
 env
+echo "exec which python"
 which python
+echo "exec python --version"
 python --version
+echo "which which pip3 && pip3 --version"
 which pip3
 pip3 --version
+echo "echo $SHELL"
+echou $SHELL
 pip3 install \
     cfx-account \
     eth-utils \
